78b93f7d0a323653a8a9ca09d91a096502b22f4a
Iana Markevitch <iana.markevitch@gmail.com>
2020-01-30 17:19:25 -0800

    Add functional nav dots to slider, change design of arrows to unicode & add image slider

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     const slides = document.getElementsByClassName('carrousel-slide');
+    const dots = document.getElementsByClassName('dot');
     const buttonRight = document.getElementById('button-right');
     const buttonLeft = document.getElementById('button-left');
 
@@ -12,12 +13,17 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (slides[i].classList.contains('visible')) {
                     slides[i].classList.toggle('visible');
                     slides[i + 1].classList.toggle('visible');
+                    // change the active dot
+                    dots[i].classList.toggle('active');
+                    dots[i + 1].classList.toggle('active');
                     break;
                 }
-            // if on the last slide, reset to slide 1
+            // if on the last slide, reset to slide 1, and reset dot
             } else {
                 slides[slides.length - 1].classList.toggle('visible');
                 slides[0].classList.toggle('visible');
+                dots[dots.length - 1].classList.toggle('active');
+                dots[0].classList.toggle('active');
             };
         };
     });
@@ -30,12 +36,17 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (slides[i].classList.contains('visible')) {
                     slides[i].classList.toggle('visible');
                     slides[i - 1].classList.toggle('visible');
+                    // change the active dot
+                    dots[i].classList.toggle('active');
+                    dots[i - 1].classList.toggle('active');
                     break;
                 }
-            // if on the first slide, reset to last slide
+            // if on the first slide, reset to last slide, and reset dot
             } else {
                 slides[0].classList.toggle('visible');
                 slides[slides.length - 1].classList.toggle('visible');
+                dots[0].classList.toggle('active');
+                dots[slides.length - 1].classList.toggle('active');
             };
         };
     });
