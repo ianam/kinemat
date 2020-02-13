d09f01be00ce1fbe3f01148cf2e81ad2b6f88568
ianam <iana.markevitch@gmail.com>
2020-02-13 08:43:48 -0800

    Add styling to contact form

--- index.js
+++ index.js
@@ -89,55 +89,55 @@ document.addEventListener('DOMContentLoaded', () => {
     /***************************/
     /* INSTAGRAM */
     /***************************/
-    const token = 'IGQVJWcFFJWWJsSGZAEazhsdXY2LU51ZA2JxV1MxQms3RkVPVklqMnFkZAmxCbjJUN1luWnE2TndiVy1fMkh0Q1hLVVdXa0lodDhWbEZAYZAnktUXF1dWdnMTFUVVFic2FMSFhOc2ZAmaE1R';
-    const app = document.getElementById('insta-feed');
-    let idArr = [];
+    // const token = 'IGQVJWcFFJWWJsSGZAEazhsdXY2LU51ZA2JxV1MxQms3RkVPVklqMnFkZAmxCbjJUN1luWnE2TndiVy1fMkh0Q1hLVVdXa0lodDhWbEZAYZAnktUXF1dWdnMTFUVVFic2FMSFhOc2ZAmaE1R';
+    // const app = document.getElementById('insta-feed');
+    // let idArr = [];
 
-    // Get posts list
-    fetch(`https://graph.instagram.com/17841419281502081/media?access_token=${token}`)
-    .then(response => {
-        return response.json();
-    })
-    .then(posts => {
-        // For each piece of data, push the post ID into our ID array
-        for (el of posts.data) {
-            idArr.push(el.id);
-        }
+    // // Get posts list
+    // fetch(`https://graph.instagram.com/17841419281502081/media?access_token=${token}`)
+    // .then(response => {
+    //     return response.json();
+    // })
+    // .then(posts => {
+    //     // For each piece of data, push the post ID into our ID array
+    //     for (el of posts.data) {
+    //         idArr.push(el.id);
+    //     }
 
-        // Fetch individual posts by ID
-        for (id of idArr) {
-            fetch(`https://graph.instagram.com/${id}?fields=id,media_type,media_url,caption,permalink,timestamp&access_token=${token}`)
-                .then(response => {
-                    return response.json();
-                })
-                .then(fields => {
-                    // Use fields to populate the page with cards
-                    const card = document.createElement('div');
-                    card.setAttribute('class', 'card');
+    //     // Fetch individual posts by ID
+    //     for (id of idArr) {
+    //         fetch(`https://graph.instagram.com/${id}?fields=id,media_type,media_url,caption,permalink,timestamp&access_token=${token}`)
+    //             .then(response => {
+    //                 return response.json();
+    //             })
+    //             .then(fields => {
+    //                 // Use fields to populate the page with cards
+    //                 const card = document.createElement('div');
+    //                 card.setAttribute('class', 'card');
 
-                    const image = document.createElement('img');
-                    image.src = fields.media_url;
+    //                 const image = document.createElement('img');
+    //                 image.src = fields.media_url;
 
-                    const link = document.createElement('a');
-                    link.href = fields.permalink;
-                    link.appendChild(image);
+    //                 const link = document.createElement('a');
+    //                 link.href = fields.permalink;
+    //                 link.appendChild(image);
 
-                    const caption = document.createElement('p');
-                    caption.textContent = fields.caption;
+    //                 const caption = document.createElement('p');
+    //                 caption.textContent = fields.caption;
 
-                    const timestamp = document.createElement('p');
-                    timestamp.textContent = fields.timestamp;
+    //                 const timestamp = document.createElement('p');
+    //                 timestamp.textContent = fields.timestamp;
 
-                    app.appendChild(card);
+    //                 app.appendChild(card);
 
-                    card.appendChild(link);
-                    card.appendChild(caption);
-                    card.appendChild(timestamp);
-                });
-        }
-    })
-    .catch(error => {
-        console.log("There was an error", error)
-    });
+    //                 card.appendChild(link);
+    //                 card.appendChild(caption);
+    //                 card.appendChild(timestamp);
+    //             });
+    //     }
+    // })
+    // .catch(error => {
+    //     console.log("There was an error", error)
+    // });
 
 });
